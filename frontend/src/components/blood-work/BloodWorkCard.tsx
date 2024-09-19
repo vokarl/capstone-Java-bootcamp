1a77f43dba001a5a79af138068654b226710f2c4
vokarl <karl.vogl@gmx.de>
2024-09-19 10:13:21 +0200

    Add DELETE request for BloodWork

--- frontend/src/components/blood-work/BloodWorkCard.tsx
+++ frontend/src/components/blood-work/BloodWorkCard.tsx
@@ -1,21 +1,43 @@
 import {BloodWork} from "../../models/blood-work.ts";
-import {Card, Typography} from "@mui/material";
+import { Button, Card, CardContent,  Typography} from "@mui/material";
 
 type BloodWorkCardProps ={
     bloodWork : BloodWork;
+    onDelete: (id: string) => void;
 }
-export default function BloodWorkCard({ bloodWork }: Readonly<BloodWorkCardProps>) {
+export default function BloodWorkCard({ bloodWork, onDelete }: Readonly<BloodWorkCardProps>) {
     console.log("Rendering BloodWorkCard with:", bloodWork);
 
     return (
-        <Card>
-
-            <Typography>GFR: {bloodWork.dateTime}</Typography>
-            <Typography>GFR: {bloodWork.gfr}</Typography>
-            <Typography>CRP: {bloodWork.crp}</Typography>
+        <Card sx={{ padding: 0,
+            display: "flex",
+            flexDirection:"row",
+            mr: 20,
+        }}>
+            <CardContent sx={{
+                padding: 1,
+                display:"flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems:"center",
+                gap: 1}}>
+                <Button
+                variant="contained"
+                color="primary"
+                onClick={()=> onDelete(bloodWork.id)}>DEL.</Button>
+                <Typography sx={styles.typography}> {bloodWork.dateTime}</Typography>
+                <Typography sx={styles.typography}>GFR: {bloodWork.gfr}</Typography>
+                <Typography sx={styles.typography}>CRP: {bloodWork.crp}</Typography>
+            </CardContent>
         </Card>
     );
 }
 
 
-
+const styles = {
+    typography: {
+        fontSize: "14px",
+        width: '150px',
+        textAlign: 'center'
+    }
+};
--- frontend/src/components/blood-work/BloodWorkList.tsx
+++ frontend/src/components/blood-work/BloodWorkList.tsx
@@ -22,18 +22,26 @@ export default function BloodWorkList(){
         fetchData()
     },[]);
 
-    const handleBloodWork = (newBloodWork: BloodWork) => {
+    const handleBloodWorkForm = (newBloodWork: BloodWork) => {
         setBloodValues(prevBloodWork =>[...prevBloodWork, newBloodWork]);
     }
-
+    const handleDelete = (id: string)=>{
+        axios.delete(`api/blood-work/${id}`)
+            .then(()=>{
+                fetchData()
+            })
+            .catch(error =>{
+                console.error("deletion not possible", error)
+            })
+    }
     return(
         <>
             <FormControl >
-                <BloodWorkForm onAddBloodWorkForm={handleBloodWork}/>
+                <BloodWorkForm onAddBloodWorkForm={handleBloodWorkForm}/>
             </FormControl>
             <h2>Bloodwork: </h2>
             {bloodValues.map((bloodWork)=>
-            <BloodWorkCard key={bloodWork.id} bloodWork={bloodWork} />
+            <BloodWorkCard key={bloodWork.id} bloodWork={bloodWork} onDelete={handleDelete}/>
             )}
         </>
     )
