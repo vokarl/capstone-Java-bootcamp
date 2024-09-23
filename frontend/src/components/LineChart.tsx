62660af6eb86b46e4eea080ce862aac284ad084d
vokarl <karl.vogl@gmx.de>
2024-09-23 09:51:25 +0200

    Add chart for bloodwork and average values

--- frontend/src/components/LineChart.tsx
+++ frontend/src/components/LineChart.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 import { ResponsiveLine } from '@nivo/line';
 import { PressureReading } from "../models/pressure-reading.ts";
 
-
 type LineChartProps = {
     readings: PressureReading[];
-
 }
 
 const LineChart: React.FC<LineChartProps> = ({ readings }) => {
@@ -13,6 +11,18 @@ const LineChart: React.FC<LineChartProps> = ({ readings }) => {
     const currentYear = readings.length > 0
         ? new Date(readings[0].dateTime).getFullYear()
         : new Date().getFullYear();
+
+    const averageBpm = readings.length > 0
+        ? (readings.reduce((sum, reading) => sum + reading.bpm, 0) / readings.length).toFixed(2)
+        : 0;
+
+    const averageSystolic = readings.length > 0
+        ? (readings.reduce((sum, reading) => sum + reading.systolic, 0) / readings.length).toFixed(2)
+        : 0;
+
+    const averageDiastolic = readings.length > 0
+        ? (readings.reduce((sum, reading) => sum + reading.diastolic, 0) / readings.length).toFixed(2)
+        : 0;
 
     const data1 = [
         { id: 'Bpm', data: readings.map(reading => ({
@@ -29,20 +39,34 @@ const LineChart: React.FC<LineChartProps> = ({ readings }) => {
             }))},
     ];
 
-   /* const data2 = [
-        { id: 'gfr', data: bloodWorkValues.map(bloodwork => ({
-                x: new Date(bloodwork.dateTime).toISOString().split('T')[0],
-                y:bloodwork.gfr
-            }))},
-        { id: 'crp', data: bloodWorkValues.map(bloodwork => ({
-                x: new Date(bloodwork.dateTime).toISOString().split('T')[0],
-                y: bloodwork.crp,
-            }))},
-    ];*/
+    const data2 = [
+        { id: 'CRP',
+            data: [
+                { x: '2024-09-12', y: 3 },
+                { x: '2024-08-20', y: 2 },
+                { x: '2024-07-04', y: 1 },
+                { x: '2024-06-11', y: 1 },
+            ],
+        },
+        {
+            id: 'GFR',
+            data: [
+                { x: '2024-09-12', y: 55 },
+                { x: '2024-08-20', y: 64 },
+                { x: '2024-07-04', y: 70 },
+                { x: '2024-06-11', y: 60 },
+            ],
+        },
+    ];
 
     return (
-        <div>
-            <div style={{ height: '400px' }}>
+        <div >
+            <div style={{height: '400px', marginBottom: '10px'}}>
+                <div style={{fontWeight: 'bold', marginBottom: '30px'}}>Blood Pressure</div>
+                <div style={{fontSize: '0.9em', marginBottom: '0px'}}>
+                    Average BPM: {averageBpm} | Average Systolic: {averageSystolic} | Average
+                    Diastolic: {averageDiastolic}
+                </div>
                 <ResponsiveLine
                     data={data1}
                     xScale={{
@@ -55,6 +79,67 @@ const LineChart: React.FC<LineChartProps> = ({ readings }) => {
                         type: 'linear',
                         min: 0,
                         max: 200,
+                    }}
+                    axisBottom={{
+                        format: '%b %d',
+                        tickValues: 'every 1 month',
+                        legend: currentYear.toString(),
+                        legendPosition: 'middle',
+                        legendOffset: 40,
+                    }}
+                    axisLeft={{
+                        tickValues: 10,
+                        legend: '',
+                        legendPosition: 'middle',
+                        legendOffset: -40,
+                    }}
+                    colors={{scheme: 'nivo'}}
+                    lineWidth={2}
+                    pointSize={10}
+                    pointColor={{theme: 'background'}}
+                    pointBorderWidth={2}
+                    pointBorderColor={{from: 'serieColor'}}
+                    useMesh={true}
+                    margin={{top: 50, right: 90, bottom: 60, left: 50}}
+                    legends={[
+                        {
+                            anchor: 'top-right',
+                            direction: 'column',
+                            translateX: 120,
+                            translateY: -30,
+                            itemsSpacing: 2,
+                            itemWidth: 100,
+                            itemHeight: 20,
+                            symbolSize: 12,
+                            symbolShape: 'circle',
+                            effects: [
+                                {
+                                    on: 'hover',
+                                    style: {
+                                        itemOpacity: 1,
+                                    },
+                                },
+                            ],
+                        },
+                    ]}
+                />
+
+            </div>
+
+            <div style={{height: '400px'}}>
+                <div style={{fontWeight: 'bold', marginBottom: '10px'}}>Bloodwork</div>
+                <ResponsiveLine
+                    data={data2}
+                    xScale={{
+                        type: 'time',
+                        format: '%Y-%m-%d',
+                        precision: 'day',
+                    }}
+                    xFormat="time:%Y-%m-%d"
+                    yScale={{
+                        type: 'linear',
+                        min: 0,
+                        max: 100,
                     }}
                     axisBottom={{
                         format: '%b %d',
@@ -100,65 +185,6 @@ const LineChart: React.FC<LineChartProps> = ({ readings }) => {
                     ]}
                 />
             </div>
-
-          {/*  <div style={{ height: '400px' }}>
-                <ResponsiveLine
-                    data={data2}
-                    xScale={{
-                        type: 'time',
-                        format: '%Y-%m-%d',
-                        precision: 'day',
-                    }}
-                    xFormat="time:%Y-%m-%d"
-                    yScale={{
-                        type: 'linear',
-                        min: 0,
-                        max: 200,
-                    }}
-                    axisBottom={{
-                        format: '%b %d',
-                        tickValues: 'every 1 month',
-                        legend: currentYear.toString(),
-                        legendPosition: 'middle',
-                        legendOffset: 40,
-                    }}
-                    axisLeft={{
-                        tickValues: 10,
-                        legend: '',
-                        legendPosition: 'middle',
-                        legendOffset: -40,
-                    }}
-                    colors={{ scheme: 'nivo' }}
-                    lineWidth={2}
-                    pointSize={10}
-                    pointColor={{ theme: 'background' }}
-                    pointBorderWidth={2}
-                    pointBorderColor={{ from: 'serieColor' }}
-                    useMesh={true}
-                    margin={{ top: 50, right: 90, bottom: 60, left: 50 }}
-                    legends={[
-                        {
-                            anchor: 'top-right',
-                            direction: 'column',
-                            translateX: 120,
-                            translateY: -30,
-                            itemsSpacing: 2,
-                            itemWidth: 100,
-                            itemHeight: 20,
-                            symbolSize: 12,
-                            symbolShape: 'circle',
-                            effects: [
-                                {
-                                    on: 'hover',
-                                    style: {
-                                        itemOpacity: 1,
-                                    },
-                                },
-                            ],
-                        },
-                    ]}
-                />
-            </div>*/}
         </div>
     );
 };
